761815b9381313ce2d681c19bbdacfa7bdcb2f91
Grimicron <111781736+Grimicron@users.noreply.github.com>
2022-09-01 11:07:51 +0000

    TODO done, fixed typos

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 $(document).ready(function() {
-    $(window).scroll( function(){
-        $('.hideme').each( function(i){
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+    $('#discord-icon').on('click', function(){
+        navigator.clipboard.writeText("Grimicron#2905");
+    });
+    $('.hideme').each(function(i) {
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight() / 5;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if (bottom_of_window > bottom_of_object){
+            $(this).animate({'opacity':'1'}, 500);
+        }
+    }); 
+    $(window).scroll(function() {
+        $('.hideme').each(function(i) {
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight() / 5;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             if (bottom_of_window > bottom_of_object){
                 $(this).animate({'opacity':'1'}, 500);
             }
         }); 
+    });
+});
+
+$(function() {
+    $('[data-toggle="popover"]').popover().click(function() {
+        setTimeout(function() {
+            $('[data-toggle="popover"]').popover('hide');
+        }, 2000);
     });
 });
